4c960d4549e496f724bce60ac11268f93cdff91b
Raul2680 <f.raul2603@gmail.com>
2025-11-15 14:17:28 +0000

    Initial Commit

---
+++ IoTDashboard/IoTDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoTDashboardApp.swift
+//  IoTDashboard
+//
+//  Created by Raul Ferreira on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct IoTDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
